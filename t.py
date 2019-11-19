e9aae07fc968c58287538a9d69159c048a7995fd
harry <h12360110@gmail.com>
2019-11-20 02:09:02 +0800

    Update t.py

--- t.py
+++ t.py
@@ -44,7 +44,7 @@ for _ in range(iters):
     cos_sim = cosine_similarity(query_vecs, doc_tfidfs)
     rankings = np.flip(cos_sim.argsort(axis=1), axis=1)
 
-'''
+
 with open('prefit.txt', mode='w') as file:
     for query_name, ranking in zip(query_list, rankings):
         ranking=ranking[:50]
@@ -57,3 +57,4 @@ with open('submission.txt', mode='w') as file:
         ranking=ranking[:50]
         ranked_docs = ' '.join([doc_list[idx] for idx in ranking])
         file.write('%s,%s\n' % (query_name, ranked_docs))
+'''
